23a30703211ef4f3c9f93213fcc2c5a777b7e7dc
eliashezron <eliashezron23@gmail.com>
2022-07-05 09:38:29 +0000

    metadataipfs upload

--- metadata.js
+++ metadata.js
@@ -7,17 +7,34 @@ let promises= []
 
 for (let i = 0; i < 11; i++) {
    promises.push(new Promise((res, rej)=>{
-        fs.readFile(`${__dirname}/nfts/output/IntergalacticCockroach #${i}/IntergalacticCockroach #${i}.json`,(err,data)=>{
+        fs.readFile(`${__dirname}/nfts/output/IntergalacticCockroach #${i}/IntergalacticCockroach #${i}.json`,(err, data)=>{
             if(err) rej()
-            ipfsArray.push(
-                JSON.stringify(data)
-              ).toString()
+            let metadata = JSON.parse(data)
+            ipfsArray.push({
+                path:`metadata/${i}.json`,
+                content: {
+                    image: `https://ipfs.moralis.io:2053/ipfs/QmRB5kMbVe27zcqUEvrY6gZDpJrSAGaFjmEa9sDSrtHUcP/images/${i}.png`,
+                    tokenId: metadata.tokenId,
+                    name: metadata.name,
+                    attributes: metadata.attributes
+                }
+            })
             res()
         })
    }))
     
 }
 Promise.all(promises).then(()=>{
-    console.log(ipfsArray)
-
+    axios.post("https://deep-index.moralis.io/api/v2/ipfs/uploadFolder", ipfsArray,
+        {
+            headers:{
+            "accept": "application/json",
+            "X-API-KEY": 'bTyKM0ql1TTNZzVFbIG9VfCOv0w1SX3Bwz9PCm31yTwqnCo7d7MqUj6EeuuONtnX', 
+            "Content-Type": "application/json "
+        }}).then((res)=>{
+                console.log(res.data)
+        }).catch((err)=>{
+                console.log(err)
+        })
 })
+// 'https://ipfs.moralis.io:2053/ipfs/QmRB5kMbVe27zcqUEvrY6gZDpJrSAGaFjmEa9sDSrtHUcP/images/9.png'
